dffc52a30ea7d422685ba47d5b21c6eccbda31ec
yuichi04 <crgone.910428.en@gmail.com>
2024-09-08 23:59:11 +0900

    メインページを追加

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -5,7 +5,7 @@ const Layout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
   return (
     <div className="flex h-screen">
       <SideMenu />
-      <main className="bg-red-300 flex-1 overflow-auto">{children}</main>
+      <main className="bg-slate-50 flex-1 overflow-auto">{children}</main>
     </div>
   );
 };
